c1631d305f2718f22dd655b98270acf5c3f56809
chou <chou>
2014-11-25 21:05:32 +0900

    fix spell

--- script/simpleTcpServer.rb
+++ script/simpleTcpServer.rb
@@ -2,10 +2,10 @@
 
 require 'socket'
 
-# 打开端口200000
+# 打开端口20000
 server = TCPServer.open(20000)
 
-#to accespt the client access
+#to aceipt the client access
 sock = server.accept
 
 #receive the data from client and print it to console 
